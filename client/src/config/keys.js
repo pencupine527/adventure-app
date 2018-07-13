2c417472af75ce6a4249eb54535de6678b548b17
pencupine527 <pencupine527.c@gmail.com>
2018-07-14 03:15:06 +0530

    Prepared For Deploy 1.0.5

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,19 +1,10 @@
-// const config = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   databaseURL: process.env.DATABASE_URL,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID
-// };
-
-var config = {
-  apiKey: 'AIzaSyBoa9XgHnxsKyGDmoafEexdtWELm2R-gIA',
-  authDomain: 'fs-database-adven-app.firebaseapp.com',
-  databaseURL: 'https://fs-database-adven-app.firebaseio.com',
-  projectId: 'fs-database-adven-app',
-  storageBucket: 'fs-database-adven-app.appspot.com',
-  messagingSenderId: '218597672148'
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 
 export default config;
