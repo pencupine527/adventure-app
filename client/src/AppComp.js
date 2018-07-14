47a088ccb7d368e7d448217df8d7abca0b4c48ea
pencupine527 <pencupine527.c@gmail.com>
2018-07-14 12:31:24 +0530

    Prepared For Deploy 1.0.7

--- client/src/AppComp.js
+++ client/src/AppComp.js
@@ -18,7 +18,16 @@ import CheckAuth from './components/common/CheckAuth';
 
 import store from './store';
 
-import config from './config/keys';
+//import config from './config/keys';
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
 
 firebase.initializeApp(config);
 
