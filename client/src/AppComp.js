dc8e114899dbb4259bdf5ad7acda3f84d1e55056
pencupine527 <pencupine527.c@gmail.com>
2018-07-14 12:58:56 +0530

    Prepared For Deploy 1.0.10

--- client/src/AppComp.js
+++ client/src/AppComp.js
@@ -20,13 +20,13 @@ import store from './store';
 
 //import config from './config/keys';
 
-const config = {
-  apiKey: AIzaSyBFgtd7GXypzezDgzpWBWvcrrypVROjoyE,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+var config = {
+  apiKey: 'AIzaSyBFgtd7GXypzezDgzpWBWvcrrypVROjoyE',
+  authDomain: 'deployed-adventursome-1.firebaseapp.com',
+  databaseURL: 'https://deployed-adventursome-1.firebaseio.com',
+  projectId: 'deployed-adventursome-1',
+  storageBucket: 'deployed-adventursome-1.appspot.com',
+  messagingSenderId: '292188550214'
 };
 
 firebase.initializeApp(config);
