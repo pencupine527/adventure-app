0a274fd2fa30a2a2ea74f15ebd71bd4a9dc0e782
pencupine527 <pencupine527.c@gmail.com>
2018-07-14 16:46:04 +0530

    Deployment 1.0.3

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,10 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY
+// };
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI:
+    'mongodb://dip:sarkar.a1996@ds261040.mlab.com:61040/devconnector-ds',
+  secretOrKey: 'Pencupine'
 };
